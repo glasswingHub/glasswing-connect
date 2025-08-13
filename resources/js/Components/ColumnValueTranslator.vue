557ed0ec112719f4b2be044fa5da167203ede7d1
Douglas Velasquez <douglasvelasquez.dev@gmail.com>
2025-08-13 09:39:45 -0600

    Replace code by labels (import records#show)

--- resources/js/Components/ColumnValueTranslator.vue
+++ resources/js/Components/ColumnValueTranslator.vue
@@ -56,9 +56,9 @@ const translateColumnValue = (col, record) => {
     case 'origin':
       return parseInt(record[col.key]) === 1 ? 'Nacional' : 'Extranjero';
     case 'typeBeneficiary':
-      return props.beneficiaryTypes.find(type => type.value === record[col.key])?.label;
+      return props.beneficiaryTypes.find(type => type.value === parseInt(record[col.key]))?.label;
     case 'volunteering_shirt_size_id':
-      return props.shirtSizes.find(size => size.value === record[col.key])?.label;
+      return props.shirtSizes.find(size => size.value === parseInt(record[col.key]))?.label;
     case 'departamento':
       return props.states.find(state => state.value === record[col.key])?.label;
     case 'municipio':
